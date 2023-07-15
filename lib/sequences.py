97c0759fc6f4249e6d21d71143d6ec793c9e89e5
Seong Kim <runmath@gmail.com>
2023-07-14 22:03:03 -0600

    test done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        f = []
+    elif length == 1:
+        f = [0]
+    elif length == 2:
+        f = [0, 1]
+    elif length > 2:
+        f = [0, 1]
+        i = 2
+        while i < length:
+            f.append(f[i-2] + f[i-1])
+            i += 1
+    print(f)
